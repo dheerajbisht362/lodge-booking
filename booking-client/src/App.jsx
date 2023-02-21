cf63ca1369eccd5557d1f58ceb96148b87517a58
DheerajBisht <dheerajbisht362@gmail.com>
2023-02-21 18:36:20 +0530

    Login-Register-Pages

--- booking-client/src/App.jsx
+++ booking-client/src/App.jsx
@@ -1,11 +1,17 @@
 import { Routes,Route } from 'react-router-dom'
 import './App.css'
+import Layout from './Layout'
 import IndexPage from './pages/indexPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
   return <Routes>
-    <Route index element={<IndexPage/>} />
-
+    <Route path="/" element={<Layout/>}>
+      <Route index element={<IndexPage/>} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<RegisterPage/>} />
+    </Route>
   </Routes>
 }
 
